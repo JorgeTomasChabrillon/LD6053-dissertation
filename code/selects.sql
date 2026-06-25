187be7cc97f448b3f65124d42f07b05cb8e432fa
Jorge Tomas Chabrillon <85903855+JorgeTomasChabrillon@users.noreply.github.com>
2026-06-25 12:27:31 +0100

    updates, added power bi

--- code/selects.sql
+++ code/selects.sql
@@ -3,3 +3,25 @@
 
 select top 5 contractAwardId,SponsorID,ContractTitle,ContractValue,SupplierName,AwardDate from ContractsFinderAward
 select top 5 projectID, SponsorID,ProjectTitle,ProjectStatus,LeadOrganisation from GtrProject
+
+
+
+
+
+
+select top 1 * from ContractsFinderAward 
+select top 1 * from GtrProject
+
+
+
+select * from vw_MainCompanies
+select * from vw_PossibleJobs
+
+
+
+
+
+
+
+select * from vw_PossibleSkills
+select * from vw_Filters
