dc76dfb4f1e9ce43b5fdb602e0acd51199521e6d
Jorge Tomas Chabrillon <85903855+JorgeTomasChabrillon@users.noreply.github.com>
2026-06-25 10:56:38 +0100

    added database login
    database login backup, and basic select from main tables

---
+++ code/selects.sql
@@ -0,0 +1,5 @@
+
+
+
+select top 5 contractAwardId,SponsorID,ContractTitle,ContractValue,SupplierName,AwardDate from ContractsFinderAward
+select top 5 projectID, SponsorID,ProjectTitle,ProjectStatus,LeadOrganisation from GtrProject
